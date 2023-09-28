008f4f2ada152e2ff7b09400c77e54b3f0e66048
Szlovák Zoltán <szlovak.zoltan1228@gmail.com>
2023-09-28 17:15:24 +0200

    Change using's order

--- Program.cs
+++ Program.cs
@@ -1,15 +1,15 @@
+global using vszk.Data;
 global using vszk.Models;
 global using vszk.Services;
+global using Microsoft.AspNetCore.Mvc;
 global using Microsoft.EntityFrameworkCore;
-global using vszk.Data;
+global using Microsoft.IdentityModel.Tokens;
+global using Microsoft.AspNetCore.Authentication.JwtBearer;
+global using System.Text;
+global using System.Security.Claims;
+global using System.IdentityModel.Tokens.Jwt;
 global using System.ComponentModel.DataAnnotations;
 global using System.ComponentModel.DataAnnotations.Schema;
-global using Microsoft.AspNetCore.Mvc;
-global using Microsoft.IdentityModel.Tokens;
-global using System.IdentityModel.Tokens.Jwt;
-global using System.Security.Claims;
-global using System.Text;
-global using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 var builder = WebApplication.CreateBuilder(args);
 
