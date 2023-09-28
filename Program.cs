60c9d5e0f1a196642cb78884e2012a4749127c3d
Szlovák Zoltán <szlovak.zoltan1228@gmail.com>
2023-09-28 17:11:50 +0200

    Add UseCors

--- Program.cs
+++ Program.cs
@@ -56,11 +56,15 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+app.UseCors(options =>
+{
+    options.AllowAnyOrigin()
+           .AllowAnyMethod()
+           .AllowAnyHeader();
+});
 
+app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
