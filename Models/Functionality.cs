f85aa95997395cf39127395f75d3236efc8e27cc
Szlovák Zoltán <szlovak.zoltan1228@gmail.com>
2023-10-20 20:18:45 +0200

    Fix the length of the attribute

--- Models/Functionality.cs
+++ Models/Functionality.cs
@@ -7,7 +7,7 @@ namespace vszk.Models
         public int FunctionalityID { get; set; }
         
         [Required]
-        [StringLength(32)]
+        [StringLength(64)]
         public string Funct { get; set; }
     }
 }
